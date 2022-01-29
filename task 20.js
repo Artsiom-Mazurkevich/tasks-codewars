7e80a6e68acc4f10addc922937935c8148b5234a
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2022-01-29 15:49:39 +0300

    task 20

---
+++ task 20.js
@@ -0,0 +1,12 @@
+//strictEqual(greet("Ryan"), "Hello, Ryan how are you doing today?");
+//strictEqual(greet("Shingles"), "Hello, Shingles how are you doing today?");
+
+
+
+
+function greet(name){
+    let sayHello = `Hello, ${name} how are you doing today?`
+    return sayHello
+  }
+
+  console.log (greet ('Rayan'))
