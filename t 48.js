aea12559f036066fa552ec9ce9e41e31b94d14ba
Artsiom-Mazurkevich <patrol1995@yandex.ru>
2023-02-03 15:04:34 +0300

    6 kyu / Are they the "same"? / ✅

---
+++ t 48.js
@@ -0,0 +1,29 @@
+// Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+//
+//     Examples
+// Valid arrays
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+
+// comp(a, b) returns true because in b 121 is the square of 11, 14641 is the square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+//
+// a = [121, 144, 19, 161, 19, 144, 19, 11]
+// b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+
+// If a or b are nil (or null or None, depending on the language), the problem doesn't make sense so return false.
+
+
+function comp(array1, array2){
+    if (!array1 || !array2) return false
+    const nums = array2.map(el => Math.sqrt(el));
+    const sum1 = array1.reduce((accum, value) => accum + value, 0)
+    const sum2 = nums.reduce((accum, value) => accum + value, 0);
+    return sum1 === sum2
+}
+
+
+comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361])
+
+
+
+
